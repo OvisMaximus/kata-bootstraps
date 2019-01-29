89dacb1dcbe228e60a22e92752601d7bba9a25d4
Lorenz <lorenz.hahn@actano.de>
2019-01-29 20:51:23 +0100

    parse 2

--- java/junit5/src/test/java/ThingTest.java
+++ java/junit5/src/test/java/ThingTest.java
@@ -6,12 +6,17 @@ public class ThingTest {
 
 
     int romanToArabic(String romanNumber) {
-        return 1;
+        return romanNumber.length();
     }
 
 
     @Test
     void it_should_parse_I_as_1() {
         assertEquals(1, romanToArabic("I"));
+    }
+
+    @Test
+    void it_should_parse_II_as_2(){
+        assertEquals(2, romanToArabic("II"));
     }
 }
