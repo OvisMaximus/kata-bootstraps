230d1b4baa422303fe21dff6c527a58c0208f7d5
Tilman Linden <tilman@monoqi.de>
2019-01-29 20:21:03 +0100

    Parse I as 1.

--- java/junit5/src/test/java/ThingTest.java
+++ java/junit5/src/test/java/ThingTest.java
@@ -6,11 +6,12 @@ public class ThingTest {
 
 
     int romanToArabic(String romanNumber) {
-        return 0;
+        return 1;
     }
 
+
     @Test
-    void it_should_take_a_string() {
-        assertEquals(0, romanToArabic("I"));
+    void it_should_parse_I_as_1() {
+        assertEquals(1, romanToArabic("I"));
     }
 }
